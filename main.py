773027d03b4b1688cd72130578c15f7cf672b49f
imchocomint <mintgaming@outlook.com.vn>
2025-08-09 13:39:11 +0700

    Patching some visual bugs

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ import argparse
 
 from rollback import *
 from getpkginp import *
-
-print("Welcome to apt-rollback.")
-print("You've decided to rollback package {pkg}.")
 pkg = getpkg()
+print("Welcome to apt-rollback.")
+print(f"You've decided to rollback package {pkg}.")
+
 version, installed_version, downgrade_version = getpkgvr(pkg)
 print("Do you want to downgrade this package? (y/n)")
 response = input().strip().lower()
 if response == 'y':
     rollback(pkg, downgrade_version)
 else:
-    print("You decided not to rollback {pkg}.")
+    print(f"You decided not to rollback {pkg}.")
     sys.exit(0)
